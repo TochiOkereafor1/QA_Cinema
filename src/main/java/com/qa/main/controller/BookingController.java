3dabb2ee1aefddf271aded94fd3766859e387496
A-Slator-QA <andrew.slator95@gmail.com>
2022-09-01 10:07:28 +0100

    created Booking service and controller classes

--- src/main/java/com/qa/main/controller/BookingController.java
+++ src/main/java/com/qa/main/controller/BookingController.java
@@ -1,5 +1,31 @@
 package com.qa.main.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.qa.main.domain.Booking;
+import com.qa.main.service.BookingService;
+
+@RestController
+@RequestMapping("/booking")
 public class BookingController {
+
+	@Autowired
+	BookingService service;
+
+	public BookingController(BookingService service) {
+		this.service = service;
+	}
+
+	@PostMapping("/createBooking")
+	public ResponseEntity<Booking> createBooking(@RequestBody Booking booking) {
+		Booking createBooking = service.createBooking(booking);
+		return new ResponseEntity<Booking>(createBooking, HttpStatus.CREATED);
+	}
 
 }
--- src/main/java/com/qa/main/service/BookingService.java
+++ src/main/java/com/qa/main/service/BookingService.java
@@ -1,5 +1,26 @@
 package com.qa.main.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.qa.main.domain.Booking;
+import com.qa.main.repository.BookingRepository;
+
+@Service
 public class BookingService {
+
+	private Booking booking;
+
+	@Autowired
+	private BookingRepository repo;
+
+	public BookingService(BookingRepository repo) {
+		this.repo = repo;
+		booking = new Booking();
+	}
+
+	public Booking createBooking(Booking booking) {
+		return repo.save(booking);
+	}
 
 }
