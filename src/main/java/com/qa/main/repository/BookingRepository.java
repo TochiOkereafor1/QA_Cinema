024cfabf135f084e28950b18484854292b0dc71c
A-Slator-QA <andrew.slator95@gmail.com>
2022-09-01 09:54:03 +0100

    created Booking Repository

--- src/main/java/com/qa/main/repository/BookingRepository.java
+++ src/main/java/com/qa/main/repository/BookingRepository.java
@@ -1,5 +1,20 @@
 package com.qa.main.repository;
 
-public interface BookingRepository {
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import com.qa.main.domain.Booking;
+
+@Repository
+public interface BookingRepository extends JpaRepository<Booking, Long> {
+
+	@Query(value = "SELECT * from Booking", nativeQuery = true)
+	public List<Booking> findAllBookings();
+
+	@Query(value = "SELECT * from Booking WHERE customerId = ?1", nativeQuery = true)
+	List<Booking> findBookingByCustomerSQL(Long customerId);
 
 }
