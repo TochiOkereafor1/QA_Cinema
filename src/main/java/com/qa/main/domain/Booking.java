5d6b1eacc3d159380b4cfef2341ac7defcb312bd
A-Slator-QA <andrew.slator95@gmail.com>
2022-09-01 10:32:13 +0100

    refactored Booking class, added lombok to pom

--- src/main/java/com/qa/main/domain/Booking.java
+++ src/main/java/com/qa/main/domain/Booking.java
@@ -1,7 +1,6 @@
 package com.qa.main.domain;
 
-import java.util.Objects;
-
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
@@ -9,15 +8,19 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Booking {
 
 	@Id
@@ -25,82 +28,27 @@ public class Booking {
 	private Long bookingID;
 
 	@NotNull
-	@Size(min = 0, max = 30)
-	@JoinColumn(name = "customerId") // FK
-	@OneToOne(targetEntity = Customer.class, fetch = FetchType.LAZY)
-	private Long customerId;
-
-	@NotNull
 	@Size(min = 0, max = 20)
 	@JoinColumn(name = "screeningId")
 	@ManyToOne(targetEntity = Screening.class, fetch = FetchType.LAZY)
-    private Long screeningId;
+	private Long screeningId;
 
-	@Autowired
-	public Booking() {
+	@Column(nullable = false)
+	private String forename;
+
+	@Column(nullable = false)
+	private String surname;
+
+	@Column(nullable = false)
+	private String emailAddress;
+
+	public Booking(@NotNull @Size(min = 0, max = 20) Long screeningId, String forename, String surname,
+			String emailAddress) {
 		super();
-	}
-
-	@Autowired
-	public Booking(Long customerId, Long screeningId) {
-		super();
-		this.customerId = customerId;
 		this.screeningId = screeningId;
-	}
-
-	@Autowired
-	public Booking(Long bookingID, Long customerId, Long screeningId) {
-		super();
-		this.bookingID = bookingID;
-		this.customerId = customerId;
-		this.screeningId = screeningId;
-	}
-
-	public Long getBookingID() {
-		return bookingID;
-	}
-
-	public void setBookingID(Long bookingID) {
-		this.bookingID = bookingID;
-	}
-
-	public Long getCustomerId() {
-		return customerId;
-	}
-
-	public void setCustomerId(Long customerId) {
-		this.customerId = customerId;
-	}
-
-	public Long getScreeningId() {
-		return screeningId;
-	}
-
-	public void setScreeningId(Long screeningId) {
-		this.screeningId = screeningId;
-	}
-
-	@Override
-	public String toString() {
-		return "Booking [bookingID=" + bookingID + ", customerId=" + customerId + ", screeningId=" + screeningId + "]";
-	}
-
-	@Override
-	public int hashCode() {
-		return Objects.hash(bookingID, customerId, screeningId);
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Booking other = (Booking) obj;
-		return Objects.equals(bookingID, other.bookingID) && Objects.equals(customerId, other.customerId)
-				&& Objects.equals(screeningId, other.screeningId);
+		this.forename = forename;
+		this.surname = surname;
+		this.emailAddress = emailAddress;
 	}
 
 }
