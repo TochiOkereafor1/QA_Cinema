d66ab3e126928f84acbecfbb22d455ae4cf4d319
Sarah Dankwa <sarah_dankwa@hotmail.co.uk>
2022-09-06 15:33:01 +0100

    Screening model tests fixed and application properties set back to test

--- src/test/java/com/qa/main/domain/ScreeningModelUnitTest.java
+++ src/test/java/com/qa/main/domain/ScreeningModelUnitTest.java
@@ -2,38 +2,61 @@ package com.qa.main.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import org.junit.jupiter.api.Test;
 
+import nl.jqno.equalsverifier.EqualsVerifier;
 
 public class ScreeningModelUnitTest {
-	
-	private static Screening testScreening; 
-	
+
+	@Test
+	public void testEquals() {
+		EqualsVerifier.simple().forClass(Screening.class).verify();
+	}
+
 	@Test
 	void testConstructor() {
-		testScreening = new Screening(1L, "14:00", 1L);
+		Screening testScreening = new Screening(1L, "14:00", 1L);
 		assertThat(testScreening).isNotNull().isInstanceOf(Screening.class);
-		assertThat(testScreening.getfilmId()).isEqualTo(1L); 
-		assertThat(testScreening.gettime()).isEqualTo("14:00"); 
-		assertThat(testScreening.getscreeningId()).isEqualTo(1L); 
+		assertThat(testScreening.getfilmId()).isEqualTo(1L);
+		assertThat(testScreening.gettime()).isEqualTo("14:00");
+		assertThat(testScreening.getscreeningId()).isEqualTo(1L);
 	}
-	
+
 	@Test
 	void testProductionConstructor() {
-		testScreening = new Screening("14:00", 1L);
+		Screening testScreening = new Screening("14:00", 1L);
 		assertThat(testScreening).isNotNull().isInstanceOf(Screening.class);
 		assertThat(testScreening.gettime()).isEqualTo("14:00");
-		assertThat(testScreening.getfilmId()).isEqualTo(1L); 		
+		assertThat(testScreening.getfilmId()).isEqualTo(1L);
 	}
-	
+
 	@Test
 	void toStringTest() {
-		testScreening = new Screening(1L, "14:00", 1L);
-		String expecting = "Screening [screeningId =" + testScreening.getfilmId() +", time=" + testScreening.gettime()+ ", screeningId=" + testScreening.getscreeningId() +  "]";
-		assertEquals(expecting,testScreening.toString());
-	
+		Screening testScreening = new Screening(1L, "14:00", 1L);
+		String expecting = "Screening [screeningId=" + testScreening.getscreeningId() + ", time="
+				+ testScreening.gettime() + ", filmId=" + testScreening.getfilmId() + "]";
+		assertEquals(expecting, testScreening.toString());
+
 	}
-	
+
+	@Test
+	public void settersTest() {
+		Screening newScreening = new Screening();
+
+		newScreening.setscreeningId(1L);
+		newScreening.settime("10:00");
+		newScreening.setfilmId(1L);
+
+		assertNotNull(newScreening.gettime());
+		assertNotNull(newScreening.getfilmId());
+		assertNotNull(newScreening.getscreeningId());
+
+		assertEquals(newScreening.getscreeningId(), 1L);
+		assertEquals(newScreening.gettime(), "10:00");
+		assertEquals(newScreening.getfilmId(), 1L);
+
+	}
 
 }
