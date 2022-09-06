7b4a053f2f36cd663f349a5363750d3737adfba0
A-Slator-QA <andrew.slator95@gmail.com>
2022-09-06 14:33:09 +0100

    functional modal, creates a booking!

--- src/main/resources/static/cinema.js
+++ src/main/resources/static/cinema.js
@@ -1,10 +1,10 @@
 
-const homeUrl = "http://localhost:8081/home";
+const homeUrl = "http://localhost:8081";
 
 const screeningTime = document.querySelector('#screeningTime');
 const model = document.querySelector('#screeningModal');
 // const adultTicket = document.querySelector("adultTicket");
-const seatRef = document.querySelector("seatRef");
+const seatRef = document.querySelector('#seatRef');
 
 const newForename = document.querySelector('#newForename');
 const newSurname = document.querySelector('#newSurname');
@@ -12,30 +12,32 @@ const newEmailAddress = document.querySelector('#newEmailAddress');
 
 
 const bookTicket = () => {
-// const adultTicketQuant = adultTicket.value;
-const newForenameBook = newForename.value;
-const newSurnameBook = newSurname.value;
-const newEmailAddressBook = newEmailAddress.value;
+    // const adultTicketQuant = adultTicket.value;
+    const newForenameBook = newForename.value;
+    const newSurnameBook = newSurname.value;
+    const newEmailAddressBook = newEmailAddress.value;
+    const seatRefBook = seatRef.value;
 
-let bookingData = {
-    "forename": newForenameBook,
-    "surname": newSurnameBook,
-    "emailAddress": newEmailAddressBook,
-}
-console.log(bookingData)
-
-fetch(`${homeUrl}/booking/createBooking`,{
-    method: "POST",
-    body: JSON.stringify(bookingData),
-    headers: {
-        "Content-Age": "application/json"
+    const bookingData = {
+        "forename": newForenameBook,
+        "surname": newSurnameBook,
+        "emailAddress": newEmailAddressBook,
+        "screeningId": seatRefBook,
     }
-})
+    console.log(bookingData)
 
-.then(response => response.json())
-.then(model => {
-    console.log(model);
-    //getBooking()
-})
-.catch(err => console.error(` ${err}`));
+    fetch(`http://localhost:8081/booking/createBooking`, {
+        method: "POST",
+        body: JSON.stringify(bookingData),
+        headers: {
+            "Content-Type": "application/json"
+        }
+    })
+
+        .then(response => response.json())
+        .then(model => {
+            console.log(model);
+            //getBooking()
+        })
+        .catch(err => console.error(` ${err}`));
 };
