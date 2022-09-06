c50e080b6e82118abbdaed1f5a71c455101a1411
A-Slator-QA <andrew.slator95@gmail.com>
2022-09-06 12:06:49 +0100

    modal functional happy time

--- src/main/resources/static/cinema.js
+++ src/main/resources/static/cinema.js
@@ -1,19 +1,41 @@
 
 const homeUrl = "http://localhost:8081/home";
 
-const screeningTime = document.querySelector("#screeningTime");
+const screeningTime = document.querySelector('#screeningTime');
 const model = document.querySelector('#screeningModal');
-const adultTicket = document.querySelector("adultTicket");
+// const adultTicket = document.querySelector("adultTicket");
 const seatRef = document.querySelector("seatRef");
+
+const newForename = document.querySelector('#newForename');
+const newSurname = document.querySelector('#newSurname');
+const newEmailAddress = document.querySelector('#newEmailAddress');
 
 
 const bookTicket = () => {
-const adultTicketQuant = adultTicket.value;
-const newSeatRef = seatRef.value;
+// const adultTicketQuant = adultTicket.value;
+const newForenameBook = newForename.value;
+const newSurnameBook = newSurname.value;
+const newEmailAddressBook = newEmailAddress.value;
 
-let ticketData = {
-    
-
+let bookingData = {
+    "forename": newForenameBook,
+    "surname": newSurnameBook,
+    "emailAddress": newEmailAddressBook,
 }
+console.log(bookingData)
 
-}
+fetch(`${homeUrl}/booking/createBooking`,{
+    method: "POST",
+    body: JSON.stringify(bookingData),
+    headers: {
+        "Content-Age": "application/json"
+    }
+})
+
+.then(response => response.json())
+.then(model => {
+    console.log(model);
+    //getBooking()
+})
+.catch(err => console.error(` ${err}`));
+};
