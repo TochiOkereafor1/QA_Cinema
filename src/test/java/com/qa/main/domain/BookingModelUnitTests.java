f98c7ec88ddf5f1599672e77d9324eb7afed44d8
A-Slator-QA <andrew.slator95@gmail.com>
2022-09-07 16:18:17 +0100

    refactored Booking classes to include ticket counts

--- src/test/java/com/qa/main/domain/BookingModelUnitTests.java
+++ src/test/java/com/qa/main/domain/BookingModelUnitTests.java
@@ -16,33 +16,39 @@ public class BookingModelUnitTests {
 
 	@Test
 	public void testBookingToString() {
-		Booking testBooking = new Booking(1L, "Richard", "Spring", "rspring@gmail.com", 1L);
+		Booking testBooking = new Booking(1L, "Richard", "Spring", "rspring@gmail.com", 1L, 1, 0, 0);
 		String expected = "Booking(bookingId=" + testBooking.getBookingId() + ", forename=" + testBooking.getForename()
 				+ ", surname=" + testBooking.getSurname() + ", emailAddress=" + testBooking.getEmailAddress()
-				+ ", screeningId=" + testBooking.getScreeningId() + ")";
+				+ ", screeningId=" + testBooking.getScreeningId() + ", adultTickets=" + testBooking.getAdultTickets()
+				+ ", childTickets=" + testBooking.getChildTickets() + ", concessionTickets="
+				+ testBooking.getConcessionTickets() + ")";
 		assertEquals(expected, testBooking.toString());
 
 	}
 
 	@Test
 	public void testBookingConstructor1() {
-		Booking testBooking = new Booking(1L, "Richard", "Spring", "rspring@gmail.com", 1L);
+		Booking testBooking = new Booking(1L, "Richard", "Spring", "rspring@gmail.com", 1L, 1, 0, 0);
 		assertEquals(Long.valueOf(1), testBooking.getBookingId());
 		assertEquals("Richard", testBooking.getForename());
 		assertEquals("Spring", testBooking.getSurname());
 		assertEquals("rspring@gmail.com", testBooking.getEmailAddress());
 		assertEquals(Long.valueOf(1), testBooking.getScreeningId());
-
+		assertEquals(1, testBooking.getAdultTickets());
+		assertEquals(0, testBooking.getChildTickets());
+		assertEquals(0, testBooking.getConcessionTickets());
 	}
 
 	@Test
 	public void testBookingConstructor2() {
-		Booking testBooking = new Booking("Richard", "Spring", "rspring@gmail.com", 1L);
+		Booking testBooking = new Booking("Richard", "Spring", "rspring@gmail.com", 1L, 1, 0, 0);
 		assertEquals("Richard", testBooking.getForename());
 		assertEquals("Spring", testBooking.getSurname());
 		assertEquals("rspring@gmail.com", testBooking.getEmailAddress());
 		assertEquals(Long.valueOf(1), testBooking.getScreeningId());
-
+		assertEquals(1, testBooking.getAdultTickets());
+		assertEquals(0, testBooking.getChildTickets());
+		assertEquals(0, testBooking.getConcessionTickets());
 	}
 
 	@Test
@@ -57,16 +63,25 @@ public class BookingModelUnitTests {
 		newBooking.setSurname("Spring");
 		newBooking.setEmailAddress("rspring@gmail.com");
 		newBooking.setScreeningId(1L);
+		newBooking.setAdultTickets(1);
+		newBooking.setChildTickets(0);
+		newBooking.setConcessionTickets(0);
 
 		assertNotNull(newBooking.getForename());
 		assertNotNull(newBooking.getSurname());
 		assertNotNull(newBooking.getEmailAddress());
 		assertNotNull(newBooking.getScreeningId());
+		assertNotNull(newBooking.getAdultTickets());
+		assertNotNull(newBooking.getChildTickets());
+		assertNotNull(newBooking.getConcessionTickets());
 
 		assertEquals(newBooking.getForename(), "Richard");
 		assertEquals(newBooking.getSurname(), "Spring");
 		assertEquals(newBooking.getEmailAddress(), "rspring@gmail.com");
 		assertEquals(newBooking.getScreeningId(), 1L);
+		assertEquals(newBooking.getAdultTickets(), 1);
+		assertEquals(newBooking.getChildTickets(), 0);
+		assertEquals(newBooking.getConcessionTickets(), 0);
 
 	}
 
